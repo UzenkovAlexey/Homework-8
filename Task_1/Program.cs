cba2039b3d3a0ae082e2ab3ddbafdbdf5dbba894
UzenLe <Uzenkovlesha@mail.ru>
2023-09-09 02:13:27 +0300

    Задача 1

---
+++ Task_1/Program.cs
@@ -0,0 +1,65 @@
+﻿
+// Задача 1:Задайте двумерный массив. 
+// Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+Console.WriteLine("Введите размер масива");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+int[,] table = new int[m, n];
+
+FillArray(table);
+PrintArray(table);
+SortNumArray(table);
+Console.WriteLine();
+PrintArray(table);
+
+void FillArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(10, 50);
+        }
+    }
+}
+
+void SortNumArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(0) - 1; k++)
+            {
+                if (array[i, k] > array[i, k + 1])
+                {
+                    int temp = array[i, k + 1];
+                    array[i, k + 1] = array[i, k];
+                    array[i, k] = temp;
+                }
+            }
+        }
+    }
+}
+
+void PrintArray(int[,] array)
+{
+    for (int l = 0; l < array.GetLength(0); l++)
+    {
+        for (int t = 0; t < array.GetLength(1); t++)
+        {
+            Console.Write($"{array[l, t]}\t");
+        }
+        Console.WriteLine();
+    }
+}
